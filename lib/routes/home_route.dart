d45ed3034242a0b0f7e0097bc1e7844ac20ddaf7
park-bohee <41457508+park-bohee@users.noreply.github.com>
2019-10-23 21:02:06 +0900

    Feat : change main banner1 picture, list tile :bug:

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import './modules/my_home.dart';
 import './modules/my_menu.dart';
 
-final Color mainColor = Color(0xff22b6ff);
 
 class HomeRoute extends StatelessWidget {
+  final Color mainColor = Color(0xff22b6ff);
+  final Color fontColor = Color(0xff3b3b3b);
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +18,7 @@ class HomeRoute extends StatelessWidget {
             color: Color(0xff3b3b3b),
           ),
         ),
+        iconTheme: IconThemeData(color: fontColor),
         backgroundColor: Color(0xffe9e9e7),
       ),
       body: MyHome(),
--- lib/routes/modules/my_menu.dart
+++ lib/routes/modules/my_menu.dart
@@ -1,39 +1,142 @@
 import 'package:flutter/material.dart';
 
-import './my_tile.dart';
+import '../contact_us_route.dart';
+import '../events_route.dart';
+import '../home_route.dart';
+import '../news_route.dart';
+import '../splash_route.dart';
 
 class MyMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Container menu = Container(
-      width: 300.0,
-      child: Column(
+    final Color fontColor = Color(0xff3b3b3b);
+
+    final Drawer menu = Drawer(
+      child: ListView(
         children: <Widget>[
           Container(
+            height: 120.0,
             color: Colors.white,
-            height: 130.0,
             child: DrawerHeader(
               margin: EdgeInsets.all(0.0),
-              padding: EdgeInsets.symmetric(
-                horizontal: 30.0
-              ),
-              child: Image.asset('assets/images/logos/logo-600.png'),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              alignment: Alignment.topLeft,
-              color: Color(0xfffafafa),
-              child: ListView.builder(
-                itemBuilder: (BuildContext context, int index) => MyTileItem(myTile[index], context),
-                itemCount: myTile.length,
-                padding: EdgeInsets.all(0.0),
+              padding: EdgeInsets.all(33.0),
+              child: Container(
+                child: Image.asset('assets/images/logos/logo-600.png'),
               ),
             ),
           ),
-        ]
-      ),
+          ListTile(
+            title: Text(
+              'about us',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.home,
+              color: fontColor,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeRoute(),
+                )
+              );
+            }
+          ),
+          ListTile(
+            title: Text(
+              'management',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.supervisor_account,
+              color: fontColor,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'trand & expo',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.language,
+              color: fontColor,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'corporate consulting',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.business,
+              color: fontColor,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'biz tour',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.flight,
+              color: fontColor,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'biz events',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.event,
+              color: fontColor,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EventsRoute(),
+                )
+              );
+            }
+          ),
+          ListTile(
+            title: Text(
+              'magazine',
+              style: TextStyle(
+                fontSize: 15.5,
+                color: fontColor
+              ),
+            ),
+            leading: Icon(
+              Icons.photo_album,
+              color: fontColor,
+            ),
+          )
+        ],
+      )
     );
 
     return menu;
